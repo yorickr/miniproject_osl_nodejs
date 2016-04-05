e842f9431b9d30208a1c766dbeb1d42b923a8ea6
Yorick <yorick-rommers@hotmail.com>
2016-04-05 12:03:52 +0000

    Added necessary important website parts

--- routes_default.js
+++ routes_default.js
@@ -5,11 +5,32 @@ var jwt = require('jwt-simple');
 var path = require('path');
 
 // Fall back, display some info
+router.get('/gaben', function (req, res) {
+    res.status(200);
+    res.type('.html');
+    res.send('<body background="http://www.imgbase.info/images/safe-wallpapers/video_games/1_other_video_games/44028_1_other_video_games_gaben.jpg"></body>');
+});
+
+
+// Fall back, display some info
+router.get('/onvoldoende', function (req, res) {
+    res.status(200);
+    res.type('.html');
+    res.send('<iframe width="560" height="315" src="https://www.youtube.com/embed/O67ni2Y2YEc" frameborder="0" allowfullscreen></iframe>');
+});
+
+// Fall back, display some info
+router.get('/kok', function (req, res) {
+    res.status(200);
+    res.type('.html');
+    res.send('<iframe width="420" height="315" src="https://www.youtube.com/embed/uBHBA2DjCpA" frameborder="0" allowfullscreen></iframe>');
+});
+
+// Fall back, display some info
 router.get('/', function (req, res) {
     res.status(200);
-    res.json({
-        "description": "Nee, ga weg"
-    });
+    res.type('.html');
+    res.send('<img src="http://puu.sh/o1Trk.png">');
 });
 
 
